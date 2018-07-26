9c0b6e3e212685025d564802556d722f18e15219
Alexander <kandallov@mac-mini-alexander.local>
2018-07-26 16:52:14 +0300

    config cell and controller

---
+++ rUSNews/View/Cells/NewsCell.swift
@@ -0,0 +1,19 @@
+//
+//  NewsCell.swift
+//  rUSNews
+//
+//  Created by Alexander Kandalov on 26.07.2018.
+//  Copyright © 2018 Alexander Kandalov. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+  @IBOutlet weak var newsImage: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var sourceLabel: UILabel!
+  
+  
+}
--- rUSNews/View/Controllers/NewsViewController.swift
+++ rUSNews/View/Controllers/NewsViewController.swift
@@ -8,16 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class NewsViewController: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var segmentCountry: UISegmentedControl!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
 
 
