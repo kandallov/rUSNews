333b7b4837375005405bb7b4febb1c4c1e4e2143
Alexander <kandallov@mac-mini-alexander.local>
2018-07-26 14:55:15 +0300

    add apiKey

---
+++ rUSNews/Model/Services/APIWorker.swift
@@ -0,0 +1,19 @@
+//
+//  APIWorker.swift
+//  rUSNews
+//
+//  Created by Alexander Kandalov on 26.07.2018.
+//  Copyright © 2018 Alexander Kandalov. All rights reserved.
+//
+
+import RxSwift
+
+protocol APIWorking {
+  
+}
+
+final class APIWorker: APIWorking {
+  
+  private let apiKey = "acdf109f02b54e84b7345254f4c910c7"
+
+}
