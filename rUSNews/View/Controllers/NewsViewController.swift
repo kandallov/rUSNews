b34c763eea0b573c499df152bac7a4bf85797434
Alexander <kandallov@mac-mini-alexander.local>
2018-07-26 17:15:11 +0300

    bind view model

--- rUSNews/View/Controllers/NewsViewController.swift
+++ rUSNews/View/Controllers/NewsViewController.swift
@@ -7,17 +7,40 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsViewController: UIViewController {
   
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var segmentCountry: UISegmentedControl!
   
+  var viewModel: NewsViewModeling!
+  private let disposeBag = DisposeBag()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 150
+    setupBindings()
+  }
+  
+  func setupBindings() {
+    segmentCountry.rx.selectedSegmentIndex
+      .bind(to: viewModel.countrySigment)
+      .disposed(by: disposeBag)
+    
+    viewModel.cellModels
+      .drive(tableView.rx.items(cellIdentifier: "NewsCell", cellType: NewsCell.self)) { index, model, cell in
+        cell.viewModel = model
+      }.disposed(by: disposeBag)
   }
 
+}
 
+extension NewsViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
 }
 
